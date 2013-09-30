43727cd6e0fd4c07c67eaf9389b843e915e1a9b3
Aha_zcj <tzl77258511@gmail.com>
2013-10-01 00:58:47 +0800

    调通登录代码

--- BabyShow/src/com/babyshow/activity/LoginActivity.java
+++ BabyShow/src/com/babyshow/activity/LoginActivity.java
@@ -2,6 +2,7 @@ package com.babyshow.activity;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -122,7 +123,7 @@ public class LoginActivity extends Activity {
 		return password.equals("");
 	}
 
-	private class LoginTask extends AsyncTask<Void, Void, Boolean> {
+	private class LoginTask extends AsyncTask<Void, Void, Integer> {
 
 		private final static String TAG = "LoginTask";
 
@@ -135,7 +136,7 @@ public class LoginActivity extends Activity {
 		}
 
 		@Override
-		protected Boolean doInBackground(Void... params) {
+		protected Integer doInBackground(Void... params) {
 
 			HttpURLConnection urlConnection = null;
 			try {
@@ -145,28 +146,42 @@ public class LoginActivity extends Activity {
 				URL url = new URL(urlString);
 				//连接服务器
 				urlConnection = (HttpURLConnection) url.openConnection();
-				//设置上传
+				
+				// 设置上传方式，注意与服务端沟通，问明白提交方式是什么，咱们这个是用post表单提交
+				urlConnection.setRequestMethod("POST");
 				urlConnection.setDoOutput(true);
-				urlConnection.setChunkedStreamingMode(0);
+				
+				// 记得重新设置超时，默认超时比较短
+				urlConnection.setReadTimeout(20 * 1000);
 				
 				OutputStream output = new BufferedOutputStream(
 						urlConnection.getOutputStream());
-				//请求数据以JSON作载体
-				JSONObject requestData = new JSONObject();
-				requestData.put("email", mEmail);
-				requestData.put("password", mPassword);
-				//将JSON写入输出流
-				output.write(requestData.toString().getBytes());
-
-				byte[] buffer = new byte[1024];
+				
+				// post提交的数据和get方式是一样的，这里和后台沟通下是否进行加密改进
+				StringBuilder sb = new StringBuilder();
+				sb.append("email=").append(mEmail).append("&password=").append(mPassword);
+				output.write(sb.toString().getBytes());
+				output.flush();
+				output.close();
+				
+				// 读取返回值，记得用循环方式来写，比较通用
 				InputStream input = new BufferedInputStream(
 						urlConnection.getInputStream());
-				input.read(buffer);
+				byte[] buffer = new byte[1024];
+				int length = 0;
+				ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+				while((length = input.read(buffer, 0, buffer.length)) > 0) {
+					outStream.write(buffer, 0, length);
+				}
+				input.close();
+				byte[] data = outStream.toByteArray();
+				String result = new String(data);
+				
 				//返回数据以JSON作载体
-				JSONObject backData = new JSONObject(buffer.toString());
-				LogUtils.debug(TAG, backData.toString());
-				return backData.getBoolean("code");
-
+				JSONObject backData = new JSONObject(result.substring(1).trim());
+				
+				// 最后返回值最好不要用boolean，通常比较常用int来表示错误码，最好整理出一套来
+				return backData.getInt("code");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -178,12 +193,13 @@ public class LoginActivity extends Activity {
 					urlConnection.disconnect();
 				}
 			}
-			return false;
+			// 应该抽取一个const类来放一些成功，失败的错误码常量，跟后台沟通一下
+			return 101;
 		}
 
 		@Override
-		protected void onPostExecute(Boolean isLogin) {
-			if (isLogin) {
+		protected void onPostExecute(Integer returnVal) {
+			if (returnVal == 0) {
 				//Lanuch the MainActivity
 				Intent intent = new Intent(LoginActivity.this,
 						MainActivity.class);
@@ -191,9 +207,10 @@ public class LoginActivity extends Activity {
 				//Destroy the loginActivity
 				finish();
 			} else {
+				// 长度没有必要就不要用自定义的吧,切记魔数，如果要自定义尽量同意写在一个地方，让后通过常量引用
 				Toast.makeText(LoginActivity.this,
 						getResources().getString(R.string.toast_validate_fail),
-						200).show();
+						Toast.LENGTH_SHORT).show();
 				;
 			}
 		}
