69575b3686b0bf37080247716d80ac0340faf995
jiaxiang.ji@gmail.com <jiaxiang.ji@gmail.com>
2013-10-04 11:17:28 +0800

    悠改LoginActivity

--- BabyShow/src/com/babyshow/activity/LoginActivity.java
+++ BabyShow/src/com/babyshow/activity/LoginActivity.java
@@ -16,6 +16,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,6 +26,7 @@ import android.widget.Toast;
 
 import com.babyshow.R;
 import com.babyshow.utils.LogUtils;
+import com.babyshow.utils.SingleThreadTask;
 
 public class LoginActivity extends Activity {
 
@@ -31,6 +34,33 @@ public class LoginActivity extends Activity {
 
 	private EditText mEmailEditText;
 	private EditText mPasswordEditText;
+	
+	private SingleThreadTask mLoginTask;
+	
+	private ValidateCompletiondHandler mHandler = new ValidateCompletiondHandler() {
+		
+		@Override
+		public void handleMessage(Message msg) {
+			if (msg.arg1 == 0) {
+				//Lanuch the MainActivity
+				Intent intent = new Intent(LoginActivity.this,
+						MainActivity.class);
+				startActivity(intent);
+				//Destroy the loginActivity
+				finish();
+			} else {
+				// 长度没有必要就不要用自定义的吧,切记魔数，如果要自定义尽量同意写在一个地方，让后通过常量引用
+				Toast.makeText(LoginActivity.this,
+						getResources().getString(R.string.toast_validate_fail),
+						Toast.LENGTH_SHORT).show();
+				;
+			}
+		}
+	};
+	
+	abstract class ValidateCompletiondHandler extends Handler{
+		public final static int MSG_VALIDATE_COMPLETION = 0;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +84,8 @@ public class LoginActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// 验证输入是否完整
-				new LoginTask().execute();
-				/*if (isLoginIdEmpty()) {
+				//new LoginTask().execute();
+				if (isEmailEmpty()) {
 					Toast.makeText(
 							LoginActivity.this,
 							getResources()
@@ -68,8 +98,11 @@ public class LoginActivity extends Activity {
 									R.string.toast_input_password), 500).show();
 				} else {
 					// 从服务器中获取验证
-					new LoginTask().execute();
-				}*/
+					doLogin();
+				}
+				
+				
+				
 			}
 		});
 		// Button注册
@@ -98,23 +131,120 @@ public class LoginActivity extends Activity {
 		});
 
 	}
+	
+	/**
+	 * 登陆：开启一个新建程作登陆
+	 */
+	public void doLogin() {
+		
+		final String email = mEmailEditText.getText().toString().trim();
+		final String password = mPasswordEditText.getText().toString().trim();
+		
+		//如果已存在正在工作的线程，停止
+		if(mLoginTask !=null) 
+			mLoginTask.stopTask();
+		//新建线程
+		mLoginTask = new SingleThreadTask() {
+
+			@Override
+			protected void task() throws Exception {
+				// TODO Auto-generated method stub
+					//通回验证结果
+					int code = validate(email, password);
+					//获得消息
+					Message msg = mHandler.obtainMessage();
+					//设置数招
+					msg.arg1 = code;
+					//发送消息
+					mHandler.sendMessage(msg);
+				}
+		};
+		//线程开始工作
+		mLoginTask.startTask();
+	}
+	
+	/**
+	 * 网络验证
+	 * @param email 登陆邮箱
+	 * @param password 登陆密码
+	 * @return 验证成功返回0，失败为101
+	 */
+	public int validate(String email, String password) {
+		
+		HttpURLConnection urlConnection = null;
+		try {
+			String urlString = getResources().getString(
+					R.string.http_do_login);
+			//新建 URL
+			URL url = new URL(urlString);
+			//连接服务器
+			urlConnection = (HttpURLConnection) url.openConnection();
+			
+			// 设置上传方式，注意与服务端沟通，问明白提交方式是什么，咱们这个是用post表单提交
+			urlConnection.setRequestMethod("POST");
+			urlConnection.setDoOutput(true);
+			
+			// 记得重新设置超时，默认超时比较短
+			urlConnection.setReadTimeout(20 * 1000);
+			
+			OutputStream output = new BufferedOutputStream(
+					urlConnection.getOutputStream());
+			
+			// post提交的数据和get方式是一样的，这里和后台沟通下是否进行加密改进
+			StringBuilder sb = new StringBuilder();
+			sb.append("email=").append(email).append("&password=").append(password);
+			output.write(sb.toString().getBytes());
+			output.flush();
+			output.close();
+			
+			// 读取返回值，记得用循环方式来写，比较通用
+			InputStream input = new BufferedInputStream(
+					urlConnection.getInputStream());
+			byte[] buffer = new byte[1024];
+			int length = 0;
+			ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+			while((length = input.read(buffer, 0, buffer.length)) > 0) {
+				outStream.write(buffer, 0, length);
+			}
+			input.close();
+			byte[] data = outStream.toByteArray();
+			String result = new String(data);
+			
+			//返回数据以JSON作载体
+			JSONObject backData = new JSONObject(result.substring(1).trim());
+			
+			// 最后返回值最好不要用boolean，通常比较常用int来表示错误码，最好整理出一套来
+			return backData.getInt("code");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (urlConnection != null)
+				urlConnection.disconnect();
+		}
+		
+		return 101;
+	}
 
 	/**
 	 * 验证 输入账号 是否为空
 	 * 
-	 * @return
+	 * @return 空则返回true, 否则返回false
 	 */
-	public boolean isLoginIdEmpty() {
+	public boolean isEmailEmpty() {
 
-		String loginId = mEmailEditText.getText().toString().trim();
+		String email = mEmailEditText.getText().toString().trim();
 
-		return loginId.equals("");
+		return email.equals("");
 	}
 
 	/**
 	 * 验证输入密码是否为空
 	 * 
-	 * @return
+	 * @return 空则返回true, 否则返回false
 	 */
 	public boolean isPasswordEmpty() {
 
@@ -123,97 +253,5 @@ public class LoginActivity extends Activity {
 		return password.equals("");
 	}
 
-	private class LoginTask extends AsyncTask<Void, Void, Integer> {
-
-		private final static String TAG = "LoginTask";
-
-		private String mEmail;
-		private String mPassword;
-
-		public LoginTask() {
-			mEmail = mEmailEditText.getText().toString().trim();
-			mPassword = mPasswordEditText.getText().toString().trim();
-		}
-
-		@Override
-		protected Integer doInBackground(Void... params) {
-
-			HttpURLConnection urlConnection = null;
-			try {
-				String urlString = getResources().getString(
-						R.string.http_do_login);
-				//新建 URL
-				URL url = new URL(urlString);
-				//连接服务器
-				urlConnection = (HttpURLConnection) url.openConnection();
-				
-				// 设置上传方式，注意与服务端沟通，问明白提交方式是什么，咱们这个是用post表单提交
-				urlConnection.setRequestMethod("POST");
-				urlConnection.setDoOutput(true);
-				
-				// 记得重新设置超时，默认超时比较短
-				urlConnection.setReadTimeout(20 * 1000);
-				
-				OutputStream output = new BufferedOutputStream(
-						urlConnection.getOutputStream());
-				
-				// post提交的数据和get方式是一样的，这里和后台沟通下是否进行加密改进
-				StringBuilder sb = new StringBuilder();
-				sb.append("email=").append(mEmail).append("&password=").append(mPassword);
-				output.write(sb.toString().getBytes());
-				output.flush();
-				output.close();
-				
-				// 读取返回值，记得用循环方式来写，比较通用
-				InputStream input = new BufferedInputStream(
-						urlConnection.getInputStream());
-				byte[] buffer = new byte[1024];
-				int length = 0;
-				ByteArrayOutputStream outStream = new ByteArrayOutputStream();
-				while((length = input.read(buffer, 0, buffer.length)) > 0) {
-					outStream.write(buffer, 0, length);
-				}
-				input.close();
-				byte[] data = outStream.toByteArray();
-				String result = new String(data);
-				
-				//返回数据以JSON作载体
-				JSONObject backData = new JSONObject(result.substring(1).trim());
-				
-				// 最后返回值最好不要用boolean，通常比较常用int来表示错误码，最好整理出一套来
-				return backData.getInt("code");
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} finally {
-				if (urlConnection != null) {
-					urlConnection.disconnect();
-				}
-			}
-			// 应该抽取一个const类来放一些成功，失败的错误码常量，跟后台沟通一下
-			return 101;
-		}
-
-		@Override
-		protected void onPostExecute(Integer returnVal) {
-			if (returnVal == 0) {
-				//Lanuch the MainActivity
-				Intent intent = new Intent(LoginActivity.this,
-						MainActivity.class);
-				startActivity(intent);
-				//Destroy the loginActivity
-				finish();
-			} else {
-				// 长度没有必要就不要用自定义的吧,切记魔数，如果要自定义尽量同意写在一个地方，让后通过常量引用
-				Toast.makeText(LoginActivity.this,
-						getResources().getString(R.string.toast_validate_fail),
-						Toast.LENGTH_SHORT).show();
-				;
-			}
-		}
-
-	}
+	
 }
